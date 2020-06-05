d605e78f99c5f97255185b12356214b636f73ede
takubt <takubt@gmail.com>
2020-06-06 03:34:30 +0900

    fs, path に置き換え

--- src/extension.ts
+++ src/extension.ts
@@ -3,43 +3,36 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 import * as moment from 'moment';
+import * as fs from 'fs';
+import * as path from 'path';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "new-folder-plus" is now active!');
-
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
-    let date = vscode.commands.registerCommand('extension.createFromDate', ({ path }) => {
+    let date = vscode.commands.registerCommand('extension.createFromDate', ({ fsPath }) => {
         // The code you place here will be executed every time your command is executed
-        console.debug('extension.createFromDate');
-        createFromDate(path);
+        createFromDate(fsPath);
     });
 
     context.subscriptions.push(date);
 
-    let clipboard = vscode.commands.registerCommand('extension.createFromClipboard', ({ path }) => {
+    let clipboard = vscode.commands.registerCommand('extension.createFromClipboard', ({ fsPath }) => {
         // The code you place here will be executed every time your command is executed
-        console.debug('extension.createFromClipboard');
-        createFromClipBoard(path);
+        createFromClipBoard(fsPath);
     });
 
     context.subscriptions.push(clipboard);
 }
 
 function createFromDate(folder: string) {
-    console.debug('createFromDate');
     let configuration = vscode.workspace.getConfiguration("newFolderPlus");
     create(folder, moment().format(configuration.get("dateFormat")));
 }
 
 function createFromClipBoard(folder: string) {
-    console.debug('createFromClipBoard');
     vscode.env.clipboard.readText().then((clipboard: string) => {
         create(folder, clipboard);
     }, (reason => {
@@ -48,13 +41,12 @@ function createFromClipBoard(folder: string) {
 }
 
 function create(folder: string, name: string) {
-    let uri = vscode.Uri.parse(folder);
-    let dest = vscode.Uri.joinPath(uri, name);
-
-    vscode.workspace.fs.createDirectory(dest).then(() => {}, reason => {
-        vscode.window.showErrorMessage('Cannot create directory', reason);
+    let dest = path.join(folder, name);
+    fs.mkdir(dest, error => {
+        if (error) {
+            vscode.window.showErrorMessage('Cannot create directory', error.message);
+        }
     });
-
 }
 
 // this method is called when your extension is deactivated
